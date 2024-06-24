6259cd0c008e91edb989e42e51ac93254e858596
Faith-Muchiri <faithwambui891@gmail.com>
2024-06-24 14:47:54 +0300

    add doctors, blogs and contact section to the homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import Services from './Services'
+import Doctors from './Doctors/Doctors'
+import Blogs from './Blogs/Blogs'
+import Contact from './Contact'
 
 const Home = () => {
   return (
     <>
       <Hero/>
       <Services/>
+      <Doctors/>
+      <Blogs/>
+      <Contact />
     </>
   )
 }
