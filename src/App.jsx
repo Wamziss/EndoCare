76471d798e708428a04f144d91bde25f48348531
Wamziss <hannahmwangi551@gmail.com>
2024-06-26 14:39:20 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,14 @@ import './App.css'
 import Layout from "./layout/Layout";
 
 function App() {
+  
   return (
+    <>
+    <p style={{backgroundColor: 'lightblue'}}>
+      Before we implement login backend, To access the dashboard append /home to the url
+    </p>
     <Layout />
+    </>
   )
 }
 
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-import Header from '../components/Header/Header'
+import Header from '../Components/Header/Header'
 import Routers from '../routes/Routers'
-import Footer from '../components/Footer/Footer'
+import Footer from '../Components/Footer/Footer'
 
 const Layout = () => {
   return (
