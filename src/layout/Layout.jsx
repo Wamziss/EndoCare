e8600102c5c25a9fb4c0926e3c2036db75e3f513
Wamziss <hannahmwangi551@gmail.com>
2024-07-16 00:47:31 +0300

    update

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Header from '../components/Header/Header'
 import Routers from '../routes/Routers'
-import Footer from '../components/Footer/Footer'it sta
+import Footer from '../components/Footer/Footer'
 
 const Layout = () => {
   return (
