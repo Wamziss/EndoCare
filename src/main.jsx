f0b4f381138328e16c9fb0de3c255de92cfcf95a
Faith-Muchiri <faithwambui891@gmail.com>
2024-07-16 11:01:05 +0300

    modify routing conflicts

--- src/main.jsx
+++ src/main.jsx
@@ -1,47 +1,61 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// import './index.css'
+
+
+// import App from './App.jsx'
+// import Home from './Components/Dashboard/Home.jsx'
+// import Records from './Components/Dashboard/Records.jsx';
+// import Health from './Components/Dashboard/Health.jsx';
+// import Community from './Components/Dashboard/Community.jsx';
+// import Saved from './Components/Dashboard/Saved.jsx';
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//   },
+//   {
+//     path: '/home',
+//     element: <Home />
+//   },
+//   {
+//     path: '/records',
+//     element: <Records />
+//   },
+//   {
+//     path: '/health',
+//     element: <Health />
+//   },
+//   {
+//     path: '/community',
+//     element: <Community />
+//   },
+//   {
+//     path: '/saved',
+//     element: <Saved />
+//   }
+// ])
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>
+// )
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-import './index.css'
-
-
 import App from './App.jsx'
-import Home from './Components/Dashboard/Home.jsx'
-import Records from './Components/Dashboard/Records.jsx';
-import Health from './Components/Dashboard/Health.jsx';
-import Community from './Components/Dashboard/Community.jsx';
-import Saved from './Components/Dashboard/Saved.jsx';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '/home',
-    element: <Home />
-  },
-  {
-    path: '/records',
-    element: <Records />
-  },
-  {
-    path: '/health',
-    element: <Health />
-  },
-  {
-    path: '/community',
-    element: <Community />
-  },
-  {
-    path: '/saved',
-    element: <Saved />
-  }
-])
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
